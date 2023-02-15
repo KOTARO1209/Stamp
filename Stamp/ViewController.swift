5a91b32a03aa15fdd9a8a4f263bb600ab8cf9a9e
KOTARO1209 <kmachida@mxdlab.net>
2023-02-15 23:07:44 +0900

    スタンプが出ない

--- Stamp/ViewController.swift
+++ Stamp/ViewController.swift
@@ -8,12 +8,50 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var imageNameArray: [String] = ["hana", "hoshi", "onpu", "shitumon"]
+    var imageIndex: Int = 0
+    
+    @IBOutlet var haikeiImageView: UIImageView!
+    
+    var imageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch: UITouch = touches.first!
+        let location: CGPoint = touch.location(in: self.view)
+        
+        if imageIndex != 0 {
+            imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
+            
+            let image: UIImage = UIImage(named: imageNameArray[imageIndex - 1])!
+            imageView.image = image
+            
+            imageView.center = CGPoint(x: location.x, y:location.y)
+            
+            self.view.addSubview(imageView)
+        }
+    }
+    
+    @IBAction func selectedFirst() {
+        imageIndex = 1
+    }
+    
+    @IBAction func selectedSecond() {
+        imageIndex = 2
+    }
+    
+    @IBAction func selectedThird() {
+        imageIndex = 3
+    }
+    
+    @IBAction func selectedFourth() {
+        imageIndex = 4
+    }
 
 }
 
